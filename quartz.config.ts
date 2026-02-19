97f5de4647436f79db5436a6c83797887db37ecc
divine <87882202+divinepablo@users.noreply.github.com>
2026-02-19 12:26:49 -0500

    Update ignorePatterns to include 'Templates'

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "divinepablo.github.io/project-icecubed/",
-    ignorePatterns: ["private", "templates", ".obsidian"],
+    ignorePatterns: ["private", "templates", "Templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
